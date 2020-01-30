9c2a277c5e731bf8599077f944e713c3f29f6861
vadim <vadikoff57@gmail.com>
2020-01-30 15:25:41 +0300

    feat: add few basic components

---
+++ src/components/weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component {
+    render(){
+        return (
+            <div>
+               Компонент с погодной информацией 
+            </div>
+        );
+    }
+}
+
+export default Info; 
