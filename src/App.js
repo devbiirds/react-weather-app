bc4b26603568a982492e7087039229d3e7da63fe
vadim <vadikoff57@gmail.com>
2020-01-30 16:18:14 +0300

    feat: add parsing json string

--- src/App.js
+++ src/App.js
@@ -7,12 +7,31 @@ const API_KEY = "4d7c525d8bf41c0284921f87d30a3c5e"
 
 class App extends React.Component {
 
+state = {
+  name:undefined,
+  country:undefined,
+  temp: undefined, 
+  sunrise:undefined,
+  sunset:undefined,
+  wind:undefined,
+  humidity:undefined
+}
 gettingWeather = async (e) => {
   e.preventDefault();
 const city_name = e.target.elements.city.value; 
+if(city_name){
 const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}`);
 const data = await api_url.json();
-console.log(data);
+this.setState({
+  name:data.name,
+  country:data.sys.country,
+  humidity:data.main.humidity,
+  wind:data.wind.speed,
+  sunrise:data.sys.sunrise,
+  sunset:data.sys.sunset
+})
+}
+
 }
 
 render() {
@@ -20,7 +39,9 @@ render() {
     <div>
       <Info />
       <Form weatherMethod = {this.gettingWeather} />
-      <Weather/>
+      <Weather 
+        info = {this.state}
+      />
     </div>
   );
 }
--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,17 @@ class Weather extends React.Component {
     render(){
         return (
             <div>
-               Компонент с погодной информацией 
+               {
+                   this.props.info.name &&
+                   <div>
+                    <p>City : {this.props.info.name} , Country : {this.props.info.country}</p>
+                    <p>Temperature : {this.props.info.temp}</p>
+                    <p>Humidity : {this.props.info.humidity} %</p>
+                    <p>Wind : {this.props.info.wind}</p>
+                    <p>Sunrise : {this.props.info.sunrise} </p>
+                    <p>Sunset : {this.props.info.sunset}</p>
+                   </div>
+               }
             </div>
         );
     }
